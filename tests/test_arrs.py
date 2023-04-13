0005e3118c4a0d0a57fc2256d4ffea6a66b4290a
Anton Parshikov <tosha1703@gmail.com>
2023-04-13 13:16:20 +0300

    2- commit. fix line 18 error

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -15,4 +15,4 @@ class TestArrs(unittest.TestCase):
         self.assertEqual(arrs.my_slice([], -1, 5), [])
         self.assertEqual(arrs.my_slice([1, 2, 3, 4], -1, 3), [])
         self.assertEqual(arrs.my_slice([1, 2, 3], -1), [3])
-        self.assertEqual(arrs.my_slice([1, 2, 3], -10), [])
+        self.assertEqual(arrs.my_slice([1, 2, 3], -10), [1, 2, 3])
